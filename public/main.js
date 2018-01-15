9a08013bedbb8ff226f950c666b0dcc823ac8f35
Emilio Villuendas <emiliovillu@gmail.com>
2018-01-15 15:35:36 +0100

    fixed url

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://192.168.0.55:3000', {'forceNew': true})
+const socket = io.connect('http://localhost:3000', {'forceNew': true})
 
 socket.on('messages', function(data){
   console.log(data)
